b1a2d892593ac3d37236d7b9096a98f03aa3d482
felix <frichardo@student.ciputra.ac.id>
2025-05-26 13:08:51 +0700

    update 26 5

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
+<?php 
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Grandia Resort</title>
 </head>
-<body>
-    
+<body class="">
+    <!-- <?php require_once("php/header.php")?> -->
+    <nav class=" bg-[] w-full h-50">
+        <h1>Grandia Resort</h1>
+    </nav>
 </body>
 </html>
