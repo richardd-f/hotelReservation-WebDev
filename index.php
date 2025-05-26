e15921c4b6f88ed3e89c8f0cd2c64328b8e9d951
felix <frichardo@student.ciputra.ac.id>
2025-05-26 16:07:48 +0700

    add tailwindcss cli

--- index.php
+++ index.php
@@ -4,15 +4,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <title>Grandia Resort</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="style/output.css" rel="stylesheet" />
+  <title>Grandia Resort</title>
 </head>
-<body class="">
-    <!-- <?php require_once("php/header.php")?> -->
-    <nav class=" bg-[] w-full h-50">
-        <h1>Grandia Resort</h1>
-    </nav>
+<body class="bg-red-500">
+  <!-- <?php require_once("php/header.php") ?> -->
+  <nav class="w-full h-[50px] flex items-center px-4 bg-gray-100">
+    <h1 class="text-xl font-bold">Grandia Resort</h1>
+  </nav>
 </body>
 </html>
