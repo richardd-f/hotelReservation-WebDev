92f2fa9523868cbb93b5443a8a9e96f589a55cf6
Drewron0 <aaronhans0606@gmail.com>
2025-06-01 16:43:04 +0700

    Rest of changes idk

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   <link href="style/output.css" rel="stylesheet" />
   <title>Grandia Resort</title>
 </head>
-<body class="bg-red-500">
+<body class="">
   <!-- <?php require_once("php/header.php") ?> -->
   <nav class="w-full h-10 flex items-center px-4 bg-gray-100">
     <h1 class="text-xl font-bold">Grandia Resort</h1>
