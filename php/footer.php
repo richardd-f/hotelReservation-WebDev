cfe737e5fd22ceba8bd68c535ca1fdf907686c8a
felix <ffelixrichardo@gmail.com>
2025-06-03 08:13:16 +0700

    update footer.php

--- php/footer.php
+++ php/footer.php
@@ -1,14 +1,39 @@
 <footer class="border-t-2 border-t-[#383838] mt-10 py-10">
-    <div class="footerTop grid grid-cols-[2fr_1fr] w-full px-10">
-        <div class="ftLeft w-full">
-            <h3>Grandia Resort</h3>
-            <p>Jl. Kartika Plaza, Kuta, Bali - Indonesia</p>
+    <div class="footerTop grid cols-1 md:grid-cols-[2fr_1fr] w-full px-10">
+        <div class="ftLeft w-full content-center">
+            <h4>Grandia Resort</h4>
+            <p>Jl. Kartika Plaza, Batu - Indonesia</p>
         </div>
 
-        <div class="ftRight w-full text-[]">
+        <div class="ftRight w-full">
             <!-- instagram sosmed -->
-            <a class="flex justify-start items-center" href="https://instagram.com/richardd._f">
-                <svg class="aspect-square mr-2 w-11 fill-current hover:text-[#00F2FF] duration-300 hover:drop-shadow-[0_0_15px_#00F2FF]" fill="#ffffff" viewBox="0 0 32 32" id="Camada_1" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <a class="flex justify-start items-center text-white hover:text-brand-gold" href="https://instagram.com/richardd._f">
+                <svg class="aspect-square mr-2 w-11 fill-current text-brand-gold hover:text-gold duration-300 hover:drop-shadow-[0_0_15px_#FFE374]" fill="#ffffff" viewBox="0 0 32 32" id="Camada_1" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <g>
+                        <path d="M22.3,8.4c-0.8,0-1.4,0.6-1.4,1.4c0,0.8,0.6,1.4,1.4,1.4c0.8,0,1.4-0.6,1.4-1.4C23.7,9,23.1,8.4,22.3,8.4z"/>
+                        <path d="M16,10.2c-3.3,0-5.9,2.7-5.9,5.9s2.7,5.9,5.9,5.9s5.9-2.7,5.9-5.9S19.3,10.2,16,10.2z M16,19.9c-2.1,0-3.8-1.7-3.8-3.8   c0-2.1,1.7-3.8,3.8-3.8c2.1,0,3.8,1.7,3.8,3.8C19.8,18.2,18.1,19.9,16,19.9z"/>
+                        <path d="M20.8,4h-9.5C7.2,4,4,7.2,4,11.2v9.5c0,4,3.2,7.2,7.2,7.2h9.5c4,0,7.2-3.2,7.2-7.2v-9.5C28,7.2,24.8,4,20.8,4z M25.7,20.8   c0,2.7-2.2,5-5,5h-9.5c-2.7,0-5-2.2-5-5v-9.5c0-2.7,2.2-5,5-5h9.5c2.7,0,5,2.2,5,5V20.8z"/>
+                    </g>
+                </svg>
+                <span>@grandia_resort</span>
+            </a> 
+
+            <!-- youtube sosmed -->
+            <a class="flex justify-start items-center text-white hover:text-brand-gold" href="https://instagram.com/richardd._f">
+                <svg class="aspect-square w-10" viewBox="0 0 461.001 461.001">
+                <g>
+                    <path style="fill:#F61C0D;" d="M365.257,67.393H95.744C42.866,67.393,0,110.259,0,163.137v134.728
+                        c0,52.878,42.866,95.744,95.744,95.744h269.513c52.878,0,95.744-42.866,95.744-95.744V163.137
+                        C461.001,110.259,418.135,67.393,365.257,67.393z M300.506,237.056l-126.06,60.123c-3.359,1.602-7.239-0.847-7.239-4.568V168.607
+                        c0-3.774,3.982-6.22,7.348-4.514l126.06,63.881C304.363,229.873,304.298,235.248,300.506,237.056z"/>
+                </g>
+                </svg>
+                <span>@grandia_resort</span>
+            </a> 
+            
+            <!-- email sosmed -->
+            <a class="flex justify-start items-center text-white hover:text-brand-gold" href="https://instagram.com/richardd._f">
+                <svg class="aspect-square mr-2 w-11 fill-current text-brand-gold hover:text-gold duration-300 hover:drop-shadow-[0_0_15px_#FFE374]" fill="#ffffff" viewBox="0 0 32 32" id="Camada_1" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                     <g>
                         <path d="M22.3,8.4c-0.8,0-1.4,0.6-1.4,1.4c0,0.8,0.6,1.4,1.4,1.4c0.8,0,1.4-0.6,1.4-1.4C23.7,9,23.1,8.4,22.3,8.4z"/>
                         <path d="M16,10.2c-3.3,0-5.9,2.7-5.9,5.9s2.7,5.9,5.9,5.9s5.9-2.7,5.9-5.9S19.3,10.2,16,10.2z M16,19.9c-2.1,0-3.8-1.7-3.8-3.8   c0-2.1,1.7-3.8,3.8-3.8c2.1,0,3.8,1.7,3.8,3.8C19.8,18.2,18.1,19.9,16,19.9z"/>
